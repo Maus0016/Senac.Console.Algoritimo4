31df9eb59dda058d23ca4e09bcce55e57efbf7db
Daniel Maus <hakumennomodo@gmail.com>
2025-04-01 21:19:29 -0300

    Salvar

--- Algoritimo4/Algoritimo4/Program.cs
+++ Algoritimo4/Algoritimo4/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+// variaveis
+double largura;
+double comprimento, area, valorMetro, valorTerreno;
+//pode fazer em mesma linha
+
+//Entrada
+largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+valorMetro = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+// Calculo
+area = largura * comprimento;
+
+valorTerreno = area * valorMetro;
+
+// saida
+Console.WriteLine("AREA - " + area.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("PRECO = " + valorTerreno.ToString("F2", CultureInfo.InvariantCulture));
+//F2 duas casas decimais
+//CultureInfo.InvariantCulture serve para trocar , por . 
